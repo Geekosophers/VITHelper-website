46fd79f62fdc15fa2b3ed1626c2bdc782eb4c047
Anshul Sachdev <anshul16.sachdev@gmal.com>
2019-05-01 00:40:05 +0530

    Fixed styles of ogin page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,12 +3,24 @@ import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
 
 export const LoginPage = ({ startLogin }) => (
-<div className="box-layout">
-  <div className="box-layout__box">
-    <h1 className="box-layout__title">VIT Helper</h1>
-    <p>VIT Helper is aimed to provide you with all the information 
-            you need to keep you all going...</p>
-    <button className="button" onClick={startLogin}>Login with Google</button>
+<div> 
+  <header className="header">
+    <div className="content-container">
+      <div className="header__content">
+        <div className="header__title">
+          <h1>VITHelper</h1>
+        </div>
+      </div>
+    </div>
+  </header>
+  <div className="box-layout">
+    <div className="box-layout__box">
+      <h1>Faculty Review</h1>
+      <img src="/images/logo.png" alt="VIT Helper Logo" />
+      <p>VIT Helper is aimed to provide you with all the information 
+              you need to keep you all going...</p>
+      <button className="button" onClick={startLogin}>Login with Google</button>
+    </div>
   </div>
 </div>
 );
