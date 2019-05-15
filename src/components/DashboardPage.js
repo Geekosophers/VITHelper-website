bcfff1e8c217493c68bcbc7da9442b09503dcc39
Anshul Sachdev <anshul16.sachdev@gmal.com>
2019-05-15 20:10:54 +0530

    added whats app links

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -2,12 +2,14 @@ import React from 'react';
 import TeachersList from './TeachersList';
 import TeachersListFilter from './TeachersListFilter';
 import Disclaimer from './Disclaimer';
+import WhatsApp from './WhatsApp';
 
 const DashboardPage = () => (
   <div>
     <Disclaimer />
     <TeachersListFilter />
     <TeachersList />
+    <WhatsApp />
   </div>
 );
 
---
+++ src/components/WhatsApp.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const WhatsApp = () => (
+  <div className="content-container">
+    <div className="list-item">
+        <p>In case you couldn't find reviews for your faculty in our database. Ask your fellow by joining our WhatsApp group from one of the following links- </p>
+        <a className="button button--link whatsapp" href="https://chat.whatsapp.com/EtxzwoNyFG62fyaDjr4rRo" target="_blank">SCOPE</a>
+        <a className="button button--link whatsapp" href="https://chat.whatsapp.com/FPOwVIPIXzlJPC0llxqypi" target="_blank">SBST</a>
+        <a className="button button--link whatsapp" href="https://chat.whatsapp.com/JphQ8x59oSuAHfrlcm0z26" target="_blank">SCE</a>
+        <a className="button button--link whatsapp" href="https://chat.whatsapp.com/DioWSSyv7If8ece5Q1q6UR" target="_blank">SCHEME</a>
+        <a className="button button--link whatsapp" href="https://chat.whatsapp.com/Fs9uRvsFPGM1HbA1LOTDPu" target="_blank">SELECT</a>
+        <a className="button button--link whatsapp" href="https://chat.whatsapp.com/HGQq0j7aSnuFrwAWy3djCf" target="_blank">SENSE</a>
+        <a className="button button--link whatsapp" href="https://chat.whatsapp.com/HrcQyHxiLU23omX0ntnU08" target="_blank">SITE</a>
+        <a className="button button--link whatsapp" href="https://chat.whatsapp.com/ETekCrXC1DAACU0HekmWR5" target="_blank">SMEC</a>
+        <a className="button button--link whatsapp" href="https://chat.whatsapp.com/Dwr3YRaSGVFIU2JXtzjoaE" target="_blank">V-SPARC</a>
+    </div>
+  </div>
+);
+
+export default WhatsApp;
