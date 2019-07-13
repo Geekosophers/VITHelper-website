adfd734376780f6123150f5e05503b3dd1befb77
Anshul Sachdev <anshul16.sachdev@gmal.com>
2019-07-13 12:58:43 +0530

    final

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -16,10 +16,16 @@ export const LoginPage = ({ startLogin }) => (
   <div className="box-layout">
     <div className="box-layout__box">
       <h1>Faculty Review</h1>
-      <img src="/images/logo.png" alt="VIT Helper Logo" />
-      <p>VIT Helper is aimed to provide you with all the information you need to keep going...</p>
+      <img src="/images/logo.png" height="120px" alt="VIT Helper Logo" />
+      {/* <p>VIT Helper is aimed to provide you with all the information you need to keep going...</p> */}
       {/* <p><b><i>Find the right faculty for you!</i></b></p> */}
-      <button className="button" onClick={startLogin}>Login with Google</button>
+      
+      <p><i>Hello Human! We hope that your Course Registration went well and you got all the desired teachers. Thankyou for becoming a part of the success of VITHelper. Wish you luck for the semester.
+VITHelper - Faculty Review was a part of the big project. We have got a lot more for you coming soon...</i></p>
+      {/* <button className="button" onClick={startLogin}>Login with Google</button> */}
+      {/* <hr /> */}
+      {/* <span>For Freshers 2k19 - </span> */}
+      {/* <a href="faqs.html"><button className="button" >FAQs</button></a> */}
     </div>
   </div>
 </div>
