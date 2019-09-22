31ffd059d1aa2b22ccb121ef4708232a38e50880
Anshul Sachdev <anshul16.sachdev@gmal.com>
2019-09-23 03:31:50 +0530

    final cat2 material

--- src/components/WhatsApp.js
+++ src/components/WhatsApp.js
@@ -3,17 +3,15 @@ import React from 'react';
 const WhatsApp = () => (
   <div className="content-container">
     <div className="list-item">
-        <p>If you weren't able to find the paper, ask your fellow mates by joining the WhatsApp group-</p>
-        <a className="button button--link whatsapp" href="https://chat.whatsapp.com/Id5QYKu7TBI3omUO3ON4gd" target="_blank">Join WhatApp Group</a>
+      {
+        window.location.pathname=="/materials" ?
+        <p>Stay up to date by joining one of the WhatsApp group-</p>
+        :
+        <p>If you weren't able to find the paper, ask your fellow mates by joining one of the WhatsApp group-</p>
+      }
+        <p><a className="button button--link whatsapp" href="https://chat.whatsapp.com/KLUtAW2rIOADjyzQPBYPAF" target="_blank">VITHelper - Papers #1</a></p>
+        <p><a className="button button--link whatsapp" href="https://chat.whatsapp.com/JKnQImP49289HdZ8UdUFKl" target="_blank">VITHelper - Papers #2</a></p>
         <p>In case of any query contact us at <b>vithelper.info@gmail.com</b></p>
-        {/* <a className="button button--link whatsapp" href="https://chat.whatsapp.com/FPOwVIPIXzlJPC0llxqypi" target="_blank">SBST</a>
-        <a className="button button--link whatsapp" href="https://chat.whatsapp.com/JphQ8x59oSuAHfrlcm0z26" target="_blank">SCE</a>
-        <a className="button button--link whatsapp" href="https://chat.whatsapp.com/DioWSSyv7If8ece5Q1q6UR" target="_blank">SCHEME</a>
-        <a className="button button--link whatsapp" href="https://chat.whatsapp.com/Fs9uRvsFPGM1HbA1LOTDPu" target="_blank">SELECT</a>
-        <a className="button button--link whatsapp" href="https://chat.whatsapp.com/HGQq0j7aSnuFrwAWy3djCf" target="_blank">SENSE</a>
-        <a className="button button--link whatsapp" href="https://chat.whatsapp.com/HrcQyHxiLU23omX0ntnU08" target="_blank">SITE</a>
-        <a className="button button--link whatsapp" href="https://chat.whatsapp.com/ETekCrXC1DAACU0HekmWR5" target="_blank">SMEC</a>
-        <a className="button button--link whatsapp" href="https://chat.whatsapp.com/Dwr3YRaSGVFIU2JXtzjoaE" target="_blank">V-SPARC</a> */}
     </div>
   </div>
 );
