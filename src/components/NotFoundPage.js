37eac207df6d8f21f500e4ea03b064a5477dcccc
Anshul Sachdev <anshul16.sachdev@gmal.com>
2019-10-01 20:55:19 +0530

    made more header changes more

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -2,15 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NotFoundPage = () => (
+  <div>
+    <header className="show-for-desktop header">
+      <div className="content-container">
+        <div className="header__content">
+          <div className="header__title">
+            <h1>VITHelper</h1>
+          </div>
+        </div>
+      </div>
+    </header>
   <div className="content-container not-found-page">
     {/* <div className="not-found-page-box"> */}
-      <p><b>404 - PAGE NOT FOUND</b></p>
+      <p className="not-found-page__content"><b>404 - PAGE NOT FOUND</b></p>
       <img src="/images/404.gif" alt="404" loop="infinite"/>
       <div className="not-found-page__content">Looks like you are in the wrong place.</div>
       <div className="not-found-page__content">Don't worry</div>
       <Link to="/"><button className="button">Click here to go back!</button></Link>
     {/* </div> */}
   </div>
+  </div> 
 );
 
 export default NotFoundPage;
