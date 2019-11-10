e69f90e306d73af312053af514a4df9bfb52adc4
Anshul Sachdev <anshul16.sachdev@gmal.com>
2019-11-10 13:18:55 +0530

    new whatsapp group added

--- src/components/WhatsApp.js
+++ src/components/WhatsApp.js
@@ -9,8 +9,9 @@ const WhatsApp = () => (
         :
         <p>If you weren't able to find the paper, ask your fellow mates by joining one of the WhatsApp group-</p>
       }
-        <p><a className="button button--link whatsapp" href="https://chat.whatsapp.com/KLUtAW2rIOADjyzQPBYPAF" target="_blank">VITHelper - Papers #1</a></p>
-        <p><a className="button button--link whatsapp" href="https://chat.whatsapp.com/JKnQImP49289HdZ8UdUFKl" target="_blank">VITHelper - Papers #2</a></p>
+        <p><a className="button button--link whatsapp" href="https://chat.whatsapp.com/KLUtAW2rIOADjyzQPBYPAF" target="_blank">VITHelper - Papers #1 (Full)</a></p>
+        <p><a className="button button--link whatsapp" href="https://chat.whatsapp.com/JKnQImP49289HdZ8UdUFKl" target="_blank">VITHelper - Papers #2 (Full)</a></p>
+        <p><a className="button button--link whatsapp" href="https://chat.whatsapp.com/K4OiQ8Z8fw1Lt9PcT9Zbvn" target="_blank">VITHelper - Papers #3</a></p>
         <p>In case of any query contact us at <b>vithelper.info@gmail.com</b></p>
     </div>
   </div>
