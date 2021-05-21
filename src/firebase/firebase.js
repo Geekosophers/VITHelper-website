d8d689c2af57f9c5b31a2ec92c9aee1a389ffe92
Anshul Sachdev <anshulsachdev@Anshuls-MacBook-Pro.local>
2021-05-21 22:31:11 +0530

    env variables setup

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyCBfxTq-enEyDNaT1mDDx_dffeaxPPtWVA",
-  authDomain: "vithelper-32e0b.firebaseapp.com",
-  databaseURL: "https://vithelper-32e0b.firebaseio.com",
-  projectId: "vithelper-32e0b",
-  storageBucket: "vithelper-32e0b.appspot.com",
-  messagingSenderId: "571499579120"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 firebase.initializeApp(config);
 
--- webpack.config.js
+++ webpack.config.js
@@ -25,10 +25,7 @@ module.exports = (env) => {
                 'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
                 'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
                 'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
-                'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID),
-                'process.env.FIREBASE_MEASUREMENT_ID': JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID),
-                'process.env.BACKEND_SERVICE_LINK': JSON.stringify(process.env.BACKEND_SERVICE_LINK),
-                'process.env.REACT_APP_GA_TRACKING_CODE': JSON.stringify(process.env.REACT_APP_GA_TRACKING_CODE)
+                'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL)
             })
         ],
         module: {
