5888bfe584cf7bb20a997cd0e369332292e41870
Anshul Sachdev <anshulsachdev@Anshuls-MacBook-Pro.local>
2021-12-14 17:53:16 +0530

    Team Building Form- Note Added for many responses

--- src/components/TeamBuildingModal.js
+++ src/components/TeamBuildingModal.js
@@ -23,15 +23,12 @@ export default class TeamBuildingModal extends React.Component{
                 >
                     <h3 className="modal__title">Join VITHelper Team</h3>
                     <div className="modal__body">
-                        <p>Hey! I hope that you are finding the platform useful. I am working on different projects like this one with a team of amazing developers and looking for more. We have a lot of ideas to work which covers the following domains-</p>
-                        <p>1. Image Processing</p>
-                        <p>2. Machine Learning</p>
-                        <p>3. App Dev</p>
-                        <p>4. Web Dev</p>
-                        <p>5. Writing blogs</p>
-                        <p>6. Designers</p>
+                        <p>
+                            Hey! I hope that you are finding the platform useful. I am working on different projects like this one with a team of amazing developers and looking for more. We have a lot of ideas to work which covers the following domains-
+                            <span style={{ fontWeight: '700' }}>Image Processing, Machine Learning, App Dev, Web Dev, Writing blogs, Designers</span>
+                        </p>
                         <p>If you want to know more fill the form and  I’ll connect with you.</p>
-                        <p><b>Note- </b>There are perks which include monetary benefits.</p>
+                        <p><span style={{ fontWeight: '700', color: '#e57670' }}><u>Note</u>- We have received many responses. Please be patient while we connect with you. Thankyou.</span></p>
                     </div>
                     <div className="modal__footer">
                         <div className="button--link"><a href="https://forms.gle/kMN9N6kzL5HaScvLA">Count me in!</a></div>
