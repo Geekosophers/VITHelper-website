ca85be01bc55211c75716d6f0fd8f3b80c475621
Shamith Rao <shamith2709@gmail.com>
2022-01-07 11:49:02 +0530

    code-rearrangement

--- src/components/CgpaCalculator.js
+++ src/components/CgpaCalculator.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
-import GpaCalculatorInput from "./GpaCalculatorInput";
-// import "./gpaCalculator.css";
+
 import { Header } from "./Header";
 import { setCgpa } from "../actions/cgpa";
 import { MatPaperButton } from "./MatPaperButton";
+import CalculatorTextInput from "./CalculatorTextInput";
+
+import InputParams from "../constants";
+
+let calculate = InputParams.calculateCgpa;
+let inputParameters = InputParams.cgpaInputParams;
 
 const CgpaCalculator = () => {
   let gpa = useSelector((state) => state.gpa);
@@ -15,90 +20,38 @@ const CgpaCalculator = () => {
   } else {
     gpa = "";
   }
-
   const dispatch = useDispatch();
-  const calculateCgpa=()=>{
-    // debugger;
-    let credsCompleted=parseFloat(document.getElementById('credsCompleted').value);
-    let currentCgpa=parseFloat(document.getElementById('currentCgpa').value);
-    let credsTaken=parseFloat(document.getElementById('credsTaken').value);
-    let gpaThisSem=parseFloat(document.getElementById('gpaThisSem').value);
-
-    if(credsCompleted<0||credsCompleted>180||currentCgpa<0||currentCgpa>10||credsTaken<0||credsTaken>27||gpaThisSem<0||gpaThisSem>10){
-      alert("Please enter Values within given Limits!");
-    }
-    else if(isNaN(credsCompleted)||isNaN(currentCgpa)||isNaN(credsTaken)||isNaN(gpaThisSem)){
-      alert("Please enter all 4 Values!")
-    }else{
-      let cgpa=(credsCompleted*currentCgpa+credsTaken*gpaThisSem)/(credsCompleted+credsTaken);
-      cgpa=cgpa.toFixed(2);
-      document.getElementById("cgpaResult").innerHTML = "CGPA : " + cgpa;
-      
-       dispatch(setCgpa(cgpa));
-    }
-    
-    console.log(credsCompleted);
-  }
-
-
+  const calculateHelper = () => {
+    let cgpa = calculate();
+    if (cgpa != null) dispatch(setCgpa(cgpa));
+  };
   return (
     <div>
       <Header></Header>
-      <MatPaperButton/>
+      <MatPaperButton />
       <div className="content-container">
         <h3 className="title">CGPA CALCULATOR</h3>
         <div className="cgpa-input-group">
           <div className="cgpa-calculator-inputs">
-            <input
-              type="number"
-              id="credsCompleted"
-              min={0}
-              max={180}
-              className="cgpa-calculator-input"
-              placeholder="Credits Completed"
-              onChange={(e) => {
-                console.log(e);
-              }}
-            />
-            <h4 className="cgpa-calculator-subscript">Maximum 180</h4>
-            <input
-              type="number"
-              id="currentCgpa"
-              min={0}
-              max={10}
-              className="cgpa-calculator-input"
-              placeholder="Current CGPA"
-              onChange={(e) => {
-                console.log(e);
-              }}
-            />
-            <h4 className="cgpa-calculator-subscript">Maximum 10</h4>
-            <input
-              type="number"
-              min={0}
-              max={27}
-              id="credsTaken"
-              className="cgpa-calculator-input"
-              placeholder="Credits taken this semester"
-              onChange={(e) => {
-                console.log(e);
-              }}
-            />
-            <h4 className="cgpa-calculator-subscript">Maximum 27</h4>
-            <input
-              type="number"
-              id="gpaThisSem"
-              min={0}
-              max={10}
-              className="cgpa-calculator-input"
-              placeholder={`GPA of this semester ${gpa}`}
-              onChange={(e) => {
-                console.log(e);
-              }}
-            />
-            <h4 className="cgpa-calculator-subscript">Maximum 10</h4>
+            {inputParameters.map((input) => {
+              return (
+                <CalculatorTextInput
+                key={input.id}
+                  id={input.id}
+                  min={input.min}
+                  max={input.max}
+                  placeholder={input.placeholder}
+                  subscript={input.subscript}
+                />
+              );
+            })}
           </div>
-          <button className="cgpaCalculateButton" onClick={calculateCgpa}>CALCULATE</button>
+          <button
+            className="cgpaCalculateButton"
+            onClick={() => calculateHelper()}
+          >
+            CALCULATE
+          </button>
           <span id="cgpaResult" className="gpaResult"></span>
         </div>
       </div>
--- src/components/MarksCalculator.js
+++ src/components/MarksCalculator.js
@@ -5,16 +5,13 @@ import GpaCalculatorInput from "./GpaCalculatorInput";
 // import "./gpaCalculator.css";
 import { Header } from "./Header";
 import { setCgpa } from "../actions/cgpa";
+import CalculatorTextInput from "./CalculatorTextInput";
 
+import InputParams from "../constants";
+
+let inputParameters = InputParams.marksInputParams;
+let calculate = InputParams.calculateMarks;
 const MarksCalculator = () => {
-  
-
-  const dispatch = useDispatch();
-  const calculateMarks=()=>{
-    
-  }
-
-
   return (
     <div>
       <Header></Header>
@@ -22,93 +19,23 @@ const MarksCalculator = () => {
         <h3 className="title">MARKS CALCULATOR</h3>
         <div className="cgpa-input-group">
           <div className="cgpa-calculator-inputs">
-            <input
-              type="number"
-              id="cat1"
-              min={0}
-              max={180}
-              className="cgpa-calculator-input"
-              placeholder="Marks in CAT1"
-              onChange={(e) => {
-                console.log(e);
-              }}
-            />
-            <h4 className="cgpa-calculator-subscript">Maximum 30</h4>
-            <input
-              type="number"
-              id="cat2"
-              min={0}
-              max={10}
-              className="cgpa-calculator-input"
-              placeholder="Marks in CAT2"
-              onChange={(e) => {
-                console.log(e);
-              }}
-            />
-            <h4 className="cgpa-calculator-subscript">Maximum 30</h4>
-            <input
-              type="number"
-              min={0}
-              max={27}
-              id="internals"
-              className="cgpa-calculator-input"
-              placeholder="Marks in Internals"
-              onChange={(e) => {
-                console.log(e);
-              }}
-            />
-            <h4 className="cgpa-calculator-subscript">Maximum 20</h4>
-            <input
-              type="number"
-              id="fat"
-              min={0}
-              max={10}
-              className="cgpa-calculator-input"
-              placeholder={`Expected Marks in FAT`}
-              onChange={(e) => {
-                console.log(e);
-              }}
-            />
-            <h4 className="cgpa-calculator-subscript">Maximum 60</h4>
-            <input
-              type="number"
-              id="lab"
-              min={0}
-              max={10}
-              className="cgpa-calculator-input"
-              placeholder={`Marks in LAB`}
-              onChange={(e) => {
-                console.log(e);
-              }}
-            />
-            <h4 className="cgpa-calculator-subscript">Maximum 100</h4>
-            <input
-              type="number"
-              id="project"
-              min={0}
-              max={10}
-              className="cgpa-calculator-input"
-              placeholder={`Marks in Project `}
-              onChange={(e) => {
-                console.log(e);
-              }}
-            />
-            <h4 className="cgpa-calculator-subscript">Maximum 100</h4>
-            <input
-              type="number"
-              id="additionalLearning"
-              min={0}
-              max={10}
-              className="cgpa-calculator-input"
-              placeholder={`Marks in Additional Learning `}
-              onChange={(e) => {
-                console.log(e);
-              }}
-            />
-            <h4 className="cgpa-calculator-subscript">Maximum 10</h4>
+            {inputParameters.map((input) => {
+              return (
+                <CalculatorTextInput
+                key={input.id}
+                  id={input.id}
+                  min={input.min}
+                  max={input.max}
+                  placeholder={input.placeholder}
+                  subscript={input.subscript}
+                />
+              );
+            })}
           </div>
-          <button className="cgpaCalculateButton" onClick={calculateMarks}>CALCULATE</button>
-          <span id="cgpaResult" className="gpaResult"></span>
+          <button className="cgpaCalculateButton" onClick={calculate}>
+            CALCULATE
+          </button>
+          <span id="marksResult" className="gpaResult"></span>
         </div>
       </div>
     </div>
