3e0a4f4386b810ac6f28018bc3f86eb929a538a0
Shikhar253 <saxenashikhar01@gmail.com>
2022-02-01 19:40:23 +0530

    hover effect changed according to device width

--- src/components/Scroll to top/ScrollButton.js
+++ src/components/Scroll to top/ScrollButton.js
@@ -1,11 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BsArrowUpCircle } from "react-icons/bs";
 import { BsFillArrowUpCircleFill } from "react-icons/bs";
 import { Button } from "./ScrollButtonStyles";
 
 const ScrollButton = () => {
+  const [isTouch, setIsTouch] = useState(false);
+  // since small screen is generally for touch-screen devies
   const [visible, setVisible] = useState(false);
-  const [isClicked, setIsClicked] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+
+  useEffect(() => {
+   ( window.innerWidth < 1100)||(document.documentElement.clientWidth<1100)||(document.body.clientWidth<1100) ? setIsTouch(true) : setIsTouch(false);
+  });
+  
   const toggleVisible = () => {
     const scrolled = document.documentElement.scrollTop;
     if (scrolled > 300) {
@@ -14,6 +21,7 @@ const ScrollButton = () => {
       setVisible(false);
     }
   };
+
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
@@ -23,19 +31,28 @@ const ScrollButton = () => {
   window.addEventListener("scroll", toggleVisible);
   return (
     <>
-      <Button
-        onMouseEnter={() => setIsClicked(true)}
-        onMouseLeave={() => setIsClicked(false)}
-      >
-        {isClicked ? (
+      {isTouch ? (
+        <Button>
           <BsFillArrowUpCircleFill
             onClick={scrollToTop}
             style={{ display: visible ? "inline" : "none" }}
           />
-        ) : (
-          <BsArrowUpCircle style={{ display: visible ? "inline" : "none" }} />
-        )}
-      </Button>
+        </Button>
+      ) : (
+        <Button
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          {isHovered ? (
+            <BsFillArrowUpCircleFill
+              onClick={scrollToTop}
+              style={{ display: visible ? "inline" : "none" }}
+            />
+          ) : (
+            <BsArrowUpCircle style={{ display: visible ? "inline" : "none" }} />
+          )}
+        </Button>
+      )}
     </>
   );
 };
