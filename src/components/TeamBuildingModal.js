12581779196839a8eaad2e5f697260081dea9143
Anshul Sachdev <anshulsachdev@Anshuls-MacBook-Pro.local>
2022-04-14 00:18:03 +0530

    Follow us on insta

--- src/components/TeamBuildingModal.js
+++ src/components/TeamBuildingModal.js
@@ -21,15 +21,15 @@ export default class TeamBuildingModal extends React.Component{
                     closeTimeoutMS={200}
                     className="modal"
                 >
-                    <h3 className="modal__title show-for-desktop" >Join Discord</h3>
-                    <h3 className="modal__title show-for-mobile" >Join Discord</h3>
+                    <h3 className="modal__title show-for-desktop" >Join Us</h3>
+                    <h3 className="modal__title show-for-mobile" >Join Us</h3>
                     <div className="modal__body">
-                        <a href="https://discord.gg/9V7xUSNDNJ">
-                            <img src='./images/join-discord-button.png' width='100%' />  
+                        <a href="https://www.instagram.com/geekosophers/">
+                            <img src='./images/insta.jpeg' width='100%' />  
                         </a>
                     </div>
                     <div className="modal__footer">
-                        <div className="button--link"><a href="https://discord.gg/9V7xUSNDNJ">Join Now!</a></div>
+                        <div className="button--link"><a href="https://www.instagram.com/geekosophers/">Let me check!</a></div>
                         <div className="button--link show-for-desktop" onClick={this.closeModal} style={{ cursor: 'pointer' }}>Maybe Later</div>
                         <div className="button--link show-for-mobile" onClick={this.closeModal} style={{ cursor: 'pointer' }}>Maybe Later</div>
                     </div>
