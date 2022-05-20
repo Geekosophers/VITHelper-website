5b0f75a15cc3e7ee2abb2633658e9c258d13b840
Shikhar253 <saxenashikhar01@gmail.com>
2022-05-21 02:49:18 +0530

    static teams page done

--- src/components/Pages/LoadingPage.js
+++ src/components/Pages/LoadingPage.js
@@ -1,10 +1,16 @@
 import React from "react";
 
 const LoadingPage = () => (
-  // <div className="loader">
-  //   <iframe src="/images/loader.html" height="90%" width="90%" style={{border:'none'}}></iframe>
-  // </div>
-  <div></div>
+  <>
+    <div className="loader">
+      <iframe
+        src="/images/loader.html"
+        height="90%"
+        width="90%"
+        style={{ border: "none" }}
+      ></iframe>
+    </div>
+  </>
 );
 
 export default LoadingPage;
--- src/components/Team.js
+++ src/components/Team.js
@@ -12,7 +12,285 @@ const Team = () => {
     <>
       <Header />
       <MatPaperButton />
-      <div className="team-main-div">Team</div>
+      <div className="team-main-div">
+        <div>
+          <div class="team-container">
+            <div class="card">
+              {/* <img src="https://my-world-bucket-alpha.s3.amazonaws.com/user_files/ID369250310000000108/profile/picture1650557439.jpg" alt="" class="story__list"> */}
+
+              <img
+                src="https://my-world-bucket-alpha.s3.amazonaws.com/user_files/ID369250310000000108/profile/picture1650557439.jpg"
+                alt="Person"
+                class="card__image"
+              />
+              <p class="card__name">Shrey Sindher</p>
+              <div class="grid-container">
+                <div class="grid-child-posts">App Developer</div>
+
+                {/* <div class="grid-child-followers">1012 Likes</div> */}
+              </div>
+              <ul class="social-icons">
+                <li>
+                  <a href="#">
+                    <i class="fa fa-instagram"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-twitter"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-linkedin"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-codepen"></i>
+                  </a>
+                </li>
+              </ul>
+              {/* <button class="btn draw-border">Follow</button> */}
+              <button class="btn draw-border">Message</button>
+            </div>
+            <div class="card">
+              {/* <img src="https://my-world-bucket-alpha.s3.amazonaws.com/user_files/ID369250310000000108/profile/picture1650557439.jpg" alt="" class="story__list"> */}
+
+              <img
+                src="https://my-world-bucket-alpha.s3.amazonaws.com/user_files/ID369250310000000108/profile/picture1650557439.jpg"
+                alt="Person"
+                class="card__image"
+              />
+              <p class="card__name">Shrey Sindher</p>
+              <div class="grid-container">
+                <div class="grid-child-posts">App Developer</div>
+
+                {/* <div class="grid-child-followers">1012 Likes</div> */}
+              </div>
+              <ul class="social-icons">
+                <li>
+                  <a href="#">
+                    <i class="fa fa-instagram"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-twitter"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-linkedin"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-codepen"></i>
+                  </a>
+                </li>
+              </ul>
+              {/* <button class="btn draw-border">Follow</button> */}
+              <button class="btn draw-border">Message</button>
+            </div>
+            <div class="card">
+              {/* <img src="https://my-world-bucket-alpha.s3.amazonaws.com/user_files/ID369250310000000108/profile/picture1650557439.jpg" alt="" class="story__list"> */}
+
+              <img
+                src="https://my-world-bucket-alpha.s3.amazonaws.com/user_files/ID369250310000000108/profile/picture1650557439.jpg"
+                alt="Person"
+                class="card__image"
+              />
+              <p class="card__name">Shrey Sindher</p>
+              <div class="grid-container">
+                <div class="grid-child-posts">App Developer</div>
+
+                {/* <div class="grid-child-followers">1012 Likes</div> */}
+              </div>
+              <ul class="social-icons">
+                <li>
+                  <a href="#">
+                    <i class="fa fa-instagram"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-twitter"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-linkedin"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-codepen"></i>
+                  </a>
+                </li>
+              </ul>
+              {/* <button class="btn draw-border">Follow</button> */}
+              <button class="btn draw-border">Message</button>
+            </div>
+            <div class="card">
+              {/* <img src="https://my-world-bucket-alpha.s3.amazonaws.com/user_files/ID369250310000000108/profile/picture1650557439.jpg" alt="" class="story__list"> */}
+
+              <img
+                src="https://my-world-bucket-alpha.s3.amazonaws.com/user_files/ID369250310000000108/profile/picture1650557439.jpg"
+                alt="Person"
+                class="card__image"
+              />
+              <p class="card__name">Shrey Sindher</p>
+              <div class="grid-container">
+                <div class="grid-child-posts">App Developer</div>
+
+                {/* <div class="grid-child-followers">1012 Likes</div> */}
+              </div>
+              <ul class="social-icons">
+                <li>
+                  <a href="#">
+                    <i class="fa fa-instagram"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-twitter"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-linkedin"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-codepen"></i>
+                  </a>
+                </li>
+              </ul>
+              {/* <button class="btn draw-border">Follow</button> */}
+              <button class="btn draw-border">Message</button>
+            </div>
+            <div class="card">
+              {/* <img src="https://my-world-bucket-alpha.s3.amazonaws.com/user_files/ID369250310000000108/profile/picture1650557439.jpg" alt="" class="story__list"> */}
+
+              <img
+                src="https://my-world-bucket-alpha.s3.amazonaws.com/user_files/ID369250310000000108/profile/picture1650557439.jpg"
+                alt="Person"
+                class="card__image"
+              />
+              <p class="card__name">Shrey Sindher</p>
+              <div class="grid-container">
+                <div class="grid-child-posts">App Developer</div>
+
+                {/* <div class="grid-child-followers">1012 Likes</div> */}
+              </div>
+              <ul class="social-icons">
+                <li>
+                  <a href="#">
+                    <i class="fa fa-instagram"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-twitter"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-linkedin"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-codepen"></i>
+                  </a>
+                </li>
+              </ul>
+              {/* <button class="btn draw-border">Follow</button> */}
+              <button class="btn draw-border">Message</button>
+            </div>
+            <div class="card">
+              {/* <img src="https://my-world-bucket-alpha.s3.amazonaws.com/user_files/ID369250310000000108/profile/picture1650557439.jpg" alt="" class="story__list"> */}
+
+              <img
+                src="https://my-world-bucket-alpha.s3.amazonaws.com/user_files/ID369250310000000108/profile/picture1650557439.jpg"
+                alt="Person"
+                class="card__image"
+              />
+              <p class="card__name">Shrey Sindher</p>
+              <div class="grid-container">
+                <div class="grid-child-posts">App Developer</div>
+
+                {/* <div class="grid-child-followers">1012 Likes</div> */}
+              </div>
+              <ul class="social-icons">
+                <li>
+                  <a href="#">
+                    <i class="fa fa-instagram"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-twitter"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-linkedin"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-codepen"></i>
+                  </a>
+                </li>
+              </ul>
+              {/* <button class="btn draw-border">Follow</button> */}
+              <button class="btn draw-border">Message</button>
+            </div>
+            <div class="card">
+              {/* <img src="https://my-world-bucket-alpha.s3.amazonaws.com/user_files/ID369250310000000108/profile/picture1650557439.jpg" alt="" class="story__list"> */}
+
+              <img
+                src="https://my-world-bucket-alpha.s3.amazonaws.com/user_files/ID369250310000000108/profile/picture1650557439.jpg"
+                alt="Person"
+                class="card__image"
+              />
+              <p class="card__name">Shrey Sindher</p>
+              <div class="grid-container">
+                <div class="grid-child-posts">App Developer</div>
+
+                {/* <div class="grid-child-followers">1012 Likes</div> */}
+              </div>
+              <ul class="social-icons">
+                <li>
+                  <a href="#">
+                    <i class="fa fa-instagram"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-twitter"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-linkedin"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-codepen"></i>
+                  </a>
+                </li>
+              </ul>
+              {/* <button class="btn draw-border">Follow</button> */}
+              <button class="btn draw-border">Message</button>
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer />
       <ScrollButton />
     </>
