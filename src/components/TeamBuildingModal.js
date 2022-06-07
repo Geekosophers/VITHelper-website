59c5cae26815070fe631b183f0172aae5856d42b
Anshul Sachdev <anshulsachdev@Anshuls-MacBook-Pro.local>
2022-06-07 22:30:07 +0530

    App published

--- src/components/TeamBuildingModal.js
+++ src/components/TeamBuildingModal.js
@@ -21,15 +21,15 @@ export default class TeamBuildingModal extends React.Component{
                     closeTimeoutMS={200}
                     className="modal"
                 >
-                    <h3 className="modal__title show-for-desktop" >Join Us</h3>
-                    <h3 className="modal__title show-for-mobile" >Join Us</h3>
+                    <h3 className="modal__title show-for-desktop" >VITHelper App</h3>
+                    <h3 className="modal__title show-for-mobile" >VITHelper App</h3>
                     <div className="modal__body">
-                        <a href="https://www.instagram.com/geekosophers/">
-                            <img src='./images/insta.jpeg' width='100%' />  
+                        <a href="https://play.google.com/store/apps/details?id=com.geekosophers.vithelper&hl=en&gl=US">
+                            <img src='./images/app.png' width='100%' />  
                         </a>
                     </div>
                     <div className="modal__footer">
-                        <div className="button--link"><a href="https://www.instagram.com/geekosophers/">Let me check!</a></div>
+                        <div className="button--link"><a href="https://play.google.com/store/apps/details?id=com.geekosophers.vithelper&hl=en&gl=US">Download Now</a></div>
                         <div className="button--link show-for-desktop" onClick={this.closeModal} style={{ cursor: 'pointer' }}>Maybe Later</div>
                         <div className="button--link show-for-mobile" onClick={this.closeModal} style={{ cursor: 'pointer' }}>Maybe Later</div>
                     </div>
